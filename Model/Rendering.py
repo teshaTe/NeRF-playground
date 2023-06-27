adc717f81d10ec2de7ac46a2a3f1c6a9f95ae3a6
teshaTe <tereshin.tesha@gmail.com>
2023-06-27 20:20:04 +0100

    [ADDED] empty class with rendering functions

---
+++ Model/Rendering.py
@@ -0,0 +1,20 @@
+import numpy as np
+import torch
+import tqdm
+
+
+class NerfRender:
+    def __init__(self):
+        pass
+
+    def generate_rays(self):
+        pass
+
+    def compute_accumulated_transmittance(self):
+        pass
+
+    def render_view(self):
+        pass
+
+    def generate_view(self):
+        pass
