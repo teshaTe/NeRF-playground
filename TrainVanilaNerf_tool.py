3ed8b2d41f826e54d6a6f2d7ef178e87a86c2d53
teshaTe <tereshin.tesha@gmail.com>
2023-06-30 11:47:35 +0100

    [UPDATED] added printing output

--- TrainVanilaNerf_tool.py
+++ TrainVanilaNerf_tool.py
@@ -60,7 +60,7 @@ if __name__ == '__main__':
     camera_intrinsic_train_data = data + "/train/intrinsics"
     camera_positional_train_data = data + "/train/pose"
     imgs_train_data = data + "/imgs/train"
-    model_name = "nerf_model_v1"
+    model_name = "nerf_model_v2"
     loss_file = "train_loss_nerf"
 
     device = "cuda"
@@ -76,29 +76,23 @@ if __name__ == '__main__':
     train = True
 
     # loading camera parameters (transforms + intrinsics), images for training the model
+    print("[INFO] Loading training data.")
     loader = dloader.DataSetLoader()
     train_imgs = loader.load_img_dataset(imgs_train_data)
     train_cam_intr = loader.load_cameras_intrinsics(camera_intrinsic_train_data)
     train_cam_poses = loader.load_cameras_positions(camera_positional_train_data)
+    print("[INFO] images arr., shape:            ", train_imgs.shape)
+    print("[INFO] cameras intr. arr., shape:     ", train_cam_intr.shape)
+    print("[INFO] cameras transform arr., shape: ", train_cam_poses.shape)
+    print("[INFO] Done.\n")
 
     # preparing data that will be used for training NeRF: arrays with cameras rays origins and direction vectors;
     # pixel arrays generated from input images
+    print("[INFO] Generating camera rays: origins & directions arrays.")
     NerfRender = render.NerfRender(img_res_x, img_res_y, num_bins, device)
     train_rays_o, train_rays_d = NerfRender.generate_rays(train_cam_poses, train_cam_intr)
     train_target_px_vals = NerfRender.generate_target_pixel_arr(train_imgs)
-
-    # preparing training data loaders
-    data_tensor = torch.cat((train_rays_o.reshape(-1, 3), train_rays_d.reshape(-1, 3), train_target_px_vals.reshape(-1, 3)), dim=1)
-    dataloader = DataLoader(data_tensor, batch_size=batch_size, shuffle=True)
-
-    # training warm up: training only the middle part of the dataset, synthetic data only
-    data_tensor_warmup = torch.cat((train_rays_o.reshape(90, 400, 400, 3)[:, 100:300, 100:300, :].reshape(-1, 3),
-                                    train_rays_d.reshape(90, 400, 400, 3)[:, 100:300, 100:300, :].reshape(-1, 3),
-                                    train_target_px_vals.reshape(90, 400, 400, 3)[:, 100:300, 100:300, :].reshape(-1, 3)), dim=1)
-    dataloader_warmup = DataLoader(data_tensor_warmup, batch_size=batch_size, shuffle=True)
-
-    # Setting up the model and it's training
-    model = NerfModel.VanillaNerfModel().to(device)
+    print("[INFO] Done.\n")
 
     model_dir = root / "torch_models"
     model_dir.mkdir(exist_ok=True)
@@ -107,19 +101,40 @@ if __name__ == '__main__':
     loss_dir.mkdir(exist_ok=True)
 
     if train:
+        print("[INFO] Start training of the model.")
+        # preparing training data loaders
+        data_tensor = torch.cat((train_rays_o.reshape(-1, 3), train_rays_d.reshape(-1, 3), train_target_px_vals.reshape(-1, 3)), dim=1)
+        dataloader = DataLoader(data_tensor, batch_size=batch_size, shuffle=True)
+
+        # training warm up: training only the middle part of the dataset, synthetic data only
+        data_tensor_warmup = torch.cat((train_rays_o.reshape(90, 400, 400, 3)[:, 100:300, 100:300, :].reshape(-1, 3),
+                                        train_rays_d.reshape(90, 400, 400, 3)[:, 100:300, 100:300, :].reshape(-1, 3),
+                                        train_target_px_vals.reshape(90, 400, 400, 3)[:, 100:300, 100:300, :].reshape(-1, 3)), dim=1)
+        dataloader_warmup = DataLoader(data_tensor_warmup, batch_size=batch_size, shuffle=True)
+
+        # Setting up the model and it's training
+        model = NerfModel.VanillaNerfModel().to(device)
+
         optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
         scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[5, 10], gamma=0.5)
 
-        training_loss, model = training(model, NerfRender, dataloader_warmup, optimizer, scheduler, tn, tf, 1, device=device)
+        _, model = training(model, NerfRender, dataloader_warmup, optimizer, scheduler, tn, tf, 1, device=device)
         training_loss, model = training(model, NerfRender, dataloader, optimizer, scheduler, tn, tf, num_epochs, device=device)
+
+        print("[INFO] Done. \n")
+        print("[INFO] Saving data.")
 
         torch.save(model.state_dict(), model_dir.as_posix() + "/" + model_name + ".pt")
         np.savetxt(loss_dir.as_posix() + "/" + loss_file + ".csv", training_loss, delimiter=',')
+        print("[INFO] Done. \n")
     else:
+        print("[INFO] Loading model: ", model_name + ".pt")
+        model = NerfModel.VanillaNerfModel().to(device)
         model.load_state_dict(torch.load(model_dir.as_posix() + "/" + model_name + ".pt"))
         training_loss = np.loadtxt(loss_dir.as_posix() + "/" + loss_file + ".csv", delimiter=',')
+        print("[INFO] Done. \n")
 
     # rendering the nerf model after training
-    img, _ = NerfRender.generate_view(model, train_rays_o[0], train_rays_d[0], tn, tf, num_bins, chunk_size)
+    img = NerfRender.generate_view(model, train_rays_o[10], train_rays_d[10], tn, tf, chunk_size)
     plt.imshow(img)
     plt.show()
