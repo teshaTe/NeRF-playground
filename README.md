3ac0e58767268b80ed1135ef48c2529e4f6246fa
Alexander Tereshin <31708304+teshaTe@users.noreply.github.com>
2023-06-30 13:30:38 +0100

    Vanila nerf (#1)
    * [ADDED] data loader
    
    * [ADDED] conda environment file
    
    * [ADDED] empty class for nerf meshing
    
    * [ADDED] empty classes with vanila NeRF models
    
    * [ADDED] empty class with rendering functions
    
    * [ADDED] empty tool for meshing NeRF
    
    * [ADDED] empty tool for training NeRF
    
    * [ADDED] empty file for futire adding of coordinate transforms between various frames of ref.
    
    * [UPDATED] Added rendering functionality for rendering the NeRF model + comments
    
    * [UPDATED] fix the typo in the name of the file
    
    * [UPDATED] number of bins now part of the constructor init
    
    * [ADDED] two files that initiate the folders as modules
    
    * [UPDATED] moved to a new folder
    
    * [UPDATED] moved to a new folder
    
    * [UPDATED] renamed the file + added implementations of the models
    
    * [UPDATED] start assembling of the training script
    
    * [UPDATED] finished training the vanilla nerf tool
    
    * [UPDATED] documentation
    
    * [UPDATED] training tool
    
    * [UPDATED] conversion to torch type + adding type conversion
    
    * [ADDED] tests for data loader and data for testing
    
    * [UPDATED] added pytest as a dependence
    
    * [UPDATED] float division
    
    * [UPDATED] fixed problem with img normalization
    
    * [REMOVED] the file
    
    * [UPDATED] added extra checks
    
    * [UPDATED] added printing output
    
    * [UPDATED] generate rays -> revert all computations to numpy
    
    * [UPDATED] added meshing implementation for the NeRF
    
    * [UPDATED] added meshing implementation for the NeRF (tool)
    
    * [UPDATED] project description

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # NeRF-playground
-NeRF implementations playground
+
+This is a pytorch-based repository with implementation of the Neural Radiance Field (NeRF). All implementations have a detailed description.
+
+### Current implementations:
+
+- Vanilla NeRF
+
+### Requirements
+
+To run the code from this repository the following is essential:
+- To run Vanilla NeRF training NVIDIA GPU will be preferable with 8 GB of RAM minimum. However, the code can be run on CPU too.
+- Conda environment: needs to be installed and initialized using environment.yml file. All dependencies will be automatically installed. 
+
+
+### Project description
+
+The repository contains two tools:
+
+- *TrainVanillaNerf_tool.py* - script for training vanilla NeRF model 
+- *MeshNerf_tool.py* - script for extracting mesh from the NeRF model. This is a very simple meshing algorithm based on mcubes package.
+
+To run both scripts you will have to open them in editor and tune parameters according to your needs.
---
+++ Utils/DataLoader.py
@@ -0,0 +1,80 @@
+import numpy as np
+import imageio
+
+from pathlib import Path
+from natsort import natsorted
+
+
+class DataSetLoader:
+    """
+    A class for loading input data for NeRF generation.
+    """
+    def __init__(self):
+        self.supported_ext = ['.png', '.jpg', '.exr']
+
+    @staticmethod
+    def load_cameras_intrinsics(files_path: str):
+        """
+        A function for loading cameras intrinsic params stored in .txt files
+        :param file_path: a string variable with path to the folder with .txt files
+        :return: a numpy ndarray (n x 4 x 4) with intrinsic data of the each input camera.
+        """
+
+        files_path = Path(files_path)
+        cam_intrinsics_files = natsorted([f for f in files_path.iterdir() if f.suffix.lower() == '.txt'], key=lambda y: y.as_posix().lower())
+
+        assert len(cam_intrinsics_files) > 0
+
+        files_number = len(cam_intrinsics_files)
+        cam_intrinsics = np.zeros((files_number, 4, 4))
+
+        for i, f in enumerate(cam_intrinsics_files):
+            cam_intrinsic_data = open(f.as_posix()).read().split()
+            cam_intrinsics[i] = np.array(cam_intrinsic_data, dtype=np.float32).reshape((4, 4))
+
+        return cam_intrinsics
+
+    @staticmethod
+    def load_cameras_positions(files_path: str):
+        """
+        A function for loading cameras transformation data (rotations + translations) stored in .txt files
+        :param file_path: a string variable with path to the folder with .txt files
+        :return: a numpy ndarray (n x 4 x 4) with transforms for each input camera.
+        """
+        files_path = Path(files_path)
+        cam_pose_files = natsorted([f for f in files_path.iterdir() if f.suffix.lower() == ".txt"], key=lambda y: y.as_posix().lower())
+
+        assert len(cam_pose_files) > 0
+
+        files_number = len(cam_pose_files)
+        cam_poses = np.zeros((files_number, 4, 4))
+
+        for i, f in enumerate(cam_pose_files):
+            cam_pose = open(f.as_posix()).read().split()
+            cam_poses[i] = np.array(cam_pose, dtype=np.float32).reshape((4, 4))
+
+        return cam_poses
+
+    def load_img_dataset(self, files_path: str):
+        """
+        A function for loading images for NeRF training.
+        :param files_path: a string variable with path to the folder with images. Check self.supported_ext for image types.
+        :return: a numpy ndarray (N x img_w x img_h x num_ch) with loaded images.
+        """
+        files_path = Path(files_path)
+        img_files = natsorted([f for f in files_path.iterdir() if f.suffix.lower() in self.supported_ext], key=lambda y: y.as_posix().lower())
+        images = []
+
+        assert len(img_files) > 0
+
+        for i, f in enumerate(img_files):
+            img = imageio.imread(f.as_posix())
+            if img.dtype == np.uint8 or img.dtype == np.uint16:
+                img = img / 255.0
+            images.append(img[None, ...])
+
+        images = np.concatenate(images)
+        if images.shape[3] == 4:
+            images = images[..., :3] * images[..., -1:] + (1 - images[..., -1:])
+
+        return images
