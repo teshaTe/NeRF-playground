e6d74f360697eefa418e8a40d12dc1ee1c6745e0
teshaTe <tereshin.tesha@gmail.com>
2023-06-30 13:08:28 +0100

    [UPDATED] added meshing implementation for the NeRF (tool)

---
+++ MeshNerf_tool.py
@@ -0,0 +1,25 @@
+import os
+import torch
+from pathlib import Path
+
+import Model.VanillaModel as VanillaNerf
+import Utils.MeshExtraction as meshing
+
+
+if __name__ == '__main__':
+    root = Path(os.path.realpath(__file__)).parent
+
+    device = "cuda"
+    grid_res = 100
+    grid_scale = 1.5
+    mcube_thres = 3.0
+
+    model_name = "nerf_model_v2"
+    model_dir = root / "torch_models"
+
+    model = VanillaNerf.VanillaNerfModel().to(device)
+    model.load_state_dict(torch.load(model_dir.as_posix() + "/" + model_name + ".pt"))
+
+    meshingsys = meshing.SimpleMeshNeRF(device)
+    vertices, faces = meshingsys.generate_mesh(model, grid_res, grid_scale, mcube_thres)
+    meshingsys.render_mesh(vertices, faces)
